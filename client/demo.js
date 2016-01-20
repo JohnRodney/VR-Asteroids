44286a3ea75bf840533a16b4e94b7f03d58029c1
John Rodney <jrodney@poeticsystems.com>
2016-01-19 23:25:21 -0600

    asteroids

---
+++ client/demo.js
@@ -0,0 +1,99 @@
+Game = {};
+Game.loaded = false;
+
+Template.scene.onRendered(function (){
+  SceneManager.init();
+  loadMeteor();
+  addStarField();
+  addCrossHair();
+  Utils.animate( [SceneManager, Utils] );
+  Utils.registerFunction(rotateAllAsteroids);
+  addMeteors();
+});
+
+function addMeteors() {
+  if(Game.loaded){ addMeteor(); }
+  setTimeout(addMeteors, Math.random()* 3000 + 1000);
+}
+
+function loadMeteor() {
+  new THREE.JSONLoader().load('/asteroid.json', saveToGame);
+}
+
+function saveToGame(geometry) {
+  var material = new THREE.MeshPhongMaterial(mapWith({map: '/astroid.jpg', bump: '/asteroidbump.jpg', spec: '/asteroidspec.jpg'}));
+  Game.asteroid = new THREE.Mesh(geometry, material);
+  Game.asteroid.name = 'asteroid';
+  Game.loaded = true;
+}
+
+function addMeteor() {
+  var newAsteroid = Game.asteroid.clone();
+  SceneManager.scene.add(newAsteroid);
+  copyRandomVector(newAsteroid.position);
+  attack(15, newAsteroid);
+}
+
+function attack(time, mesh) {
+  Utils.transition({
+    mesh: mesh,
+    type: 'vector-move',
+    opts: { stop: { y: 0, x: 0, z: 0, }, },
+    duration: time,
+    callback: function(tar){SceneManager.scene.remove(tar)}
+  });
+}
+
+function randomPosition() {
+  var pos = Math.random() * 400 - 200;
+  if(pos >= -50 || pos <= 50){
+    pos += 100;
+  }
+  return pos;
+}
+
+function copyRandomVector(target) {
+  target.x = randomPosition();
+  target.y = randomPosition();
+  target.z = randomPosition();
+}
+
+function rotateAllAsteroids() {
+  SceneManager.scene.children.forEach( function(child){
+    if(child.name === 'asteroid'){
+      child.rotation.x += .01;
+      child.rotation.y += .01;
+    }
+  });
+}
+
+function followCamera(mesh) {
+  mesh.position.z = SceneManager.raycaster.ray.direction.z;
+  mesh.position.x = SceneManager.raycaster.ray.direction.x;
+  mesh.position.y = SceneManager.raycaster.ray.direction.y;
+  mesh.lookAt( SceneManager.camera.position );
+}
+
+function addStarField() {
+  var geometry = new THREE.SphereGeometry( 200, 60, 40 );
+  geometry.applyMatrix( new THREE.Matrix4().makeScale( -2, 2, 2 ) );
+  SceneManager.scene.add( new THREE.Mesh( geometry, new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('/space.jpg') })))
+}
+
+function addCrossHair() {
+  var geometry = new THREE.CircleGeometry( 0.01 , 64 );
+  geometry.vertices.shift();
+  Game.crosshair = new THREE.Line(geometry, new THREE.LineBasicMaterial({ color: 0xff0000 }));
+  SceneManager.scene.add(Game.crosshair);
+  Game.crosshair.functionID = Utils.registerFunction(followCamera, Game.crosshair);
+}
+
+function mapWith(paths) {
+  return {
+    map: THREE.ImageUtils.loadTexture(paths.map),
+    bumpMap: THREE.ImageUtils.loadTexture(paths.bump),
+    bumpScale: 1,
+    specularMap: THREE.ImageUtils.loadTexture(paths.spec),
+    specular: new THREE.Color(1.0, 1.0, 1.0),
+  }
+}
---
+++ client/events.js
@@ -0,0 +1,66 @@
+Utils.events({
+  'lookAt .asteroid': function(mesh) {
+    SceneManager.scene.remove(mesh);
+    explosion(mesh);
+  }
+});
+
+function getScaleMap() {
+  return { 0: 0.07, 1: 0.07, 2: 0.07, 3: 0.07, 4: 0.22, 5: 0.50 };
+}
+
+function multiplyScale(mesh, index) {
+  return getScaleMap()[index] * mesh.scale.x;
+}
+
+function explosion(mesh) {
+  var clones = [];
+  for(var i = 0; i < 6; i++){
+    clones.push(spawnClone(mesh, i));
+    runAway(clones[i]);
+  }
+  registerAsAsteroids(clones, 500)
+}
+
+function registerAsAsteroids(clones, delay) {
+  setTimeout(function(){
+    clones.forEach( function(clone){
+      clone.name = 'asteroid';
+    });
+  }, delay);
+}
+
+function runAway(clone) {
+  Utils.transition({
+    mesh: clone,
+    type: 'vector-move',
+    opts: {
+      stop: {
+        y: clone.position.y + Math.random() * 200 - 100,
+        x: clone.position.x + Math.random() * 200 - 100,
+        z: clone.position.z + Math.random() * 200 - 100,
+      },
+    },
+    duration: 3,
+    callback: destroyMesh,
+  });
+}
+
+function destroyMesh(mesh) {
+  SceneManager.scene.remove(mesh);
+}
+
+function spawnClone(mesh, i) {
+  var clone = Game.asteroid.clone()
+  clone.name = 'spawning';
+  positionCopy(clone, mesh);
+  clone.scale.set(multiplyScale(mesh, i), multiplyScale(mesh, i), multiplyScale(mesh, i));
+  SceneManager.scene.add(clone);
+  return clone;
+}
+
+function positionCopy(target, source){
+  for(var prop in source.position){
+    target.position[prop] = source.position[prop];
+  }
+}
